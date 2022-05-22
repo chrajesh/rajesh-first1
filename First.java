48e2ed8da7dada5d5f6da615a6a2f9da77770673
rajesh <rajesh.chaganti18@gmail.com>
2022-05-22 11:44:59 +0530

    updated file

--- First.java
+++ First.java
@@ -1,6 +1,13 @@
-for(i=0;i<=a.length-1;i++){
-	int a=a[i];
-	int b=a[a.length-i-1];
-	a[i]=b;
-	a[a.length-i]=a;
+class First
+{
+		public static void main (String args[])
+		{
+			for(i=0;i<=a.length-1;i++){
+			int a=a[i];
+			int b=a[a.length-i-1];
+			a[i]=b;
+			a[a.length-i]=a;
+}		
+		}
 }
+
